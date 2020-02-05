a6c0dcaa01653db5559dbcf149f1211e8c8262dd
Allan Carvalho <allandcarv@gmail.com>
2020-02-05 21:43:43 +0000

    PDO - DAO - INSERT

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,96 +1,137 @@
 <?php
-  class Usuario {
+class Usuario
+{
 
-    private $idusuario;
-    private $deslogin;
-    private $dessenha;
-    private $dtcadastro;
+  private $idusuario;
+  private $deslogin;
+  private $dessenha;
+  private $dtcadastro;
 
-    public function getIdusuario() {
-      return $this->idusuario;
-    } 
-    public function setIdusuario($id): void {
-      $this->idusuario = $id;
-    }
+  public function getIdusuario()
+  {
+    return $this->idusuario;
+  }
+  public function setIdusuario($id): void
+  {
+    $this->idusuario = $id;
+  }
 
-    public function getDeslogin() {
-      return $this->deslogin;
-    } 
-    public function setDeslogin($login): void {
-      $this->deslogin = $login;
-    }
+  public function getDeslogin()
+  {
+    return $this->deslogin;
+  }
+  public function setDeslogin($login): void
+  {
+    $this->deslogin = $login;
+  }
 
-    public function getDessenha() {
-      return $this->dessenha;
-    } 
-    public function setDessenha($pass): void {
-      $this->dessenha = $pass;
-    }
+  public function getDessenha()
+  {
+    return $this->dessenha;
+  }
+  public function setDessenha($pass): void
+  {
+    $this->dessenha = $pass;
+  }
 
-    public function getDtcadastro() {
-      return $this->dtcadastro;
-    } 
-    public function setDtcadastro($date): void {
-      $this->dtcadastro = $date;
-    }
+  public function getDtcadastro()
+  {
+    return $this->dtcadastro;
+  }
+  public function setDtcadastro($date): void
+  {
+    $this->dtcadastro = $date;
+  }
 
-    public function loadById($id) {
+  public function loadById($id)
+  {
 
-      $sql = new Sql();
+    $sql = new Sql();
 
-      $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario=:ID", array(":ID"=>$id));
-      
-      if (count($results)) {        
-        $row = $results[0];
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario=:ID", array(":ID" => $id));
 
-        $this->setIdusuario($row['idusuario']);
-        $this->setDeslogin($row['deslogin']);
-        $this->setDessenha($row['dessenha']);
-        $this->setDtcadastro(new DateTime($row['dtcadastro']));
-      }
-    }
+    if (count($results)) {
+      $row = $results[0];
 
-    public static function getList() {
-      $sql = new Sql();
-
-      return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
-    }
-
-    public static function search($login) {
-      $sql = new Sql();
-
-      return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", 
-        array(":SEARCH"=> "%" . $login . "%"));
-    }
-
-    public function login($login, $senha) {
-      $sql = new Sql();
-
-      $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin=:LOGIN AND dessenha=:SENHA",
-        array(
-          ":LOGIN"=> $login,
-          ":SENHA"=> $senha
-        ));
-
-        if (count($result)) {        
-          $row = $result[0];
-  
-          $this->setIdusuario($row['idusuario']);
-          $this->setDeslogin($row['deslogin']);
-          $this->setDessenha($row['dessenha']);
-          $this->setDtcadastro(new DateTime($row['dtcadastro']));
-        } else {
-          throw new Exception("Login e/ou senha inválidos");
-        }
-    }
-
-    public function __toString():string {
-      return json_encode(array(
-        "idusuario"=>$this->getIdusuario(),
-        "deslogin"=>$this->getDeslogin(),
-        "dessenha"=>$this->getDessenha(),
-        "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-      ));
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
     }
   }
-?>
+
+  public static function getList()
+  {
+    $sql = new Sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+  }
+
+  public static function search($login)
+  {
+    $sql = new Sql();
+
+    return $sql->select(
+      "SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",
+      array(":SEARCH" => "%" . $login . "%")
+    );
+  }
+
+  public function login($login, $senha)
+  {
+    $sql = new Sql();
+
+    $result = $sql->select(
+      "SELECT * FROM tb_usuarios WHERE deslogin=:LOGIN AND dessenha=:SENHA",
+      array(
+        ":LOGIN" => $login,
+        ":SENHA" => $senha
+      )
+    );
+
+    if (count($result)) {
+      $row = $result[0];
+
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else {
+      throw new Exception("Login e/ou senha inválidos");
+    }
+  }
+
+  public function insert()
+  {
+    $sql = new Sql();
+
+    $results = $sql->select(
+      "CALL sp_usuarios_insert(:LOGIN, :PASSWORD)",
+      array(
+        ":LOGIN" => $this->getDeslogin(),
+        ":PASSWORD" => $this->getDessenha(),
+      )
+    );
+
+    if (count($results)) {
+      $row = $results[0];
+
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else {
+      throw new Exception("Login e/ou senha inválidos");
+    }
+  }
+
+  public function __toString(): string
+  {
+    return json_encode(array(
+      "idusuario" => $this->getIdusuario(),
+      "deslogin" => $this->getDeslogin(),
+      "dessenha" => $this->getDessenha(),
+      "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
+    ));
+  }
+}
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-  require_once("config.php");
+require_once("config.php");
 
-  //$root = new Usuario();
-  //$root->loadById("1");
-  //echo $root;
+//$root = new Usuario();
+//$root->loadById("1");
+//echo $root;
 
-  //$lista = Usuario::getList();
-  //echo json_encode($lista);
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-  //$search = Usuario::search("user");
-  //echo json_encode($search);
+//$search = Usuario::search("user");
+//echo json_encode($search);
 
-  $login = new Usuario();
-  $login->login("root", "12345678");
-  echo $login;
-?>
+//$login = new Usuario();
+//$login->login("root", "12345678");
+//echo $login;
+
+$aluno = new Usuario();
+$aluno->setDeslogin('aluno');
+$aluno->setDessenha('123456');
+$aluno->insert();
+
+echo $aluno;
